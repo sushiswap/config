af856023e648bd8cec95268e4c968e181f4ee52b
Matthew Lilley <hello@matthewlilley.com>
2021-02-20 01:52:00 +0000

    feat(eslint-plugin): minimal eslint config

--- packages/eslint-config/index.ts
+++ packages/eslint-config/index.ts
@@ -1,7 +1,15 @@
 import { Linter } from 'eslint'
 
 const config: Linter.Config = {
-
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
 }
 
 export default config
