8cedf88a1ca253d4e47fffa6c7662e9d53b7befd
Matthew Lilley <hello@matthewlilley.com>
2021-06-26 03:08:58 +0100

    feat: initial work on config generation

---
+++ packages/hardhat-config/generate.ts
@@ -0,0 +1,35 @@
+import { readFile, writeFile } from "fs/promises";
+
+import fetch from "node-fetch";
+import isEqual from "lodash/isEqual";
+import { resolve } from "path";
+
+// 1. Get chain data and store it
+// 2. Use chain data to fill out networks array of hh config
+// 3. ...
+
+(async () => {
+  // Fetch JSON from https://chainid.network/chains.json
+  const res = await fetch("https://chainid.network/chains.json");
+
+  if (res.status === 200 && res.statusText === "OK") {
+    const latest = await res.json();
+
+    const path = resolve(__dirname, "data/chains.json");
+
+    try {
+      await readFile(path);
+    } catch (error) {
+      await writeFile(path, JSON.stringify(latest, null, 2));
+    }
+
+    const stored = await readFile(path, { encoding: "utf8" });
+
+    if (isEqual(stored, latest)) {
+      console.log("Chains data is up to date");
+    } else {
+      console.log("Chains data needs updating...");
+      await writeFile(path, JSON.stringify(latest, null, 2));
+    }
+  }
+})();
